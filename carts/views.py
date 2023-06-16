e4b02ee2f57ca6bc84e80d2d3ac9fcee8982846d
dahymond <ssdahymond@gmail.com>
2023-06-15 20:56:41 -0500

    add the remove feacture button

--- carts/views.py
+++ carts/views.py
@@ -70,6 +70,13 @@ def remove_cart(request, product_id):
         cart_items.save()
     else:
         cart_items.delete()
-        
+
     return redirect('cart')
-    
+
+def remove_cart_item(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    cart_items = CartItem.objects.get(product=product, cart=cart)
+    cart_items.delete()
+    return redirect('cart')
+
