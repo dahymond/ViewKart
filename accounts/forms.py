e07bacafadcbdbbf6d8aed2e8ea28f6feb98738b
dahymond <ssdahymond@gmail.com>
2023-06-29 11:04:02 -0500

    fixed cart counter bug

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Account
 
 
-class RegistrationForm(forms.modelform):
+class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number', 'email', 'passowrd']
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']  
