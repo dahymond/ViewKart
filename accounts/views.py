0d30bae7b68f24b2c460e4af53e21a09dae44ab7
dahymond <ssdahymond@gmail.com>
2023-07-03 23:16:51 -0500

    completed messaging alert for the registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            first_name = form.clean_data('first_name')
-            last_name = form.clean_data('last_name')
-            phone_number = form.clean_data('phone_number')
-            email = form.clean_data('email')
-            password = form.clean_data('password')
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
             username = email.split("@")[0]
 
             user = Account.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name, username=username)
             user.phone_number = phone_number
             user.save()
+            messages.success(request, 'Registration Successful.')
+            return redirect('register')
     else:
         form = RegistrationForm()
     context = {
